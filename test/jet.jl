d097c2d807ac0c3e2fe4a0ae3bb21b9cbdd148fd
Morten Piibeleht <morten.piibeleht@juliahub.com>
2023-07-05 16:36:21 +1200

    run :basic too, in informational mode

--- test/jet.jl
+++ test/jet.jl
@@ -1,6 +1,9 @@
 import JET, JuliaHub
 using Test
 
+jet_mode = Symbol(get(ENV, "JULIAHUB_TEST_JET", "typo"))
+@info "Running JET.jl in mode=:$(jet_mode)"
+
 @testset "JET" begin
-    JET.test_package("JuliaHub"; target_defined_modules=true, mode=:typo)
+    JET.test_package("JuliaHub"; target_defined_modules=true, mode=jet_mode)
 end
