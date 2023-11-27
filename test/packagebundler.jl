67c71faf53c5a1ca871f88870b1d890b4a4108d2
Morten Piibeleht <morten.piibeleht@juliahub.com>
2023-11-27 21:52:29 +1300

    fix: make sure that created `Project.toml` gets included in the appbundle (#44)
    * test: rename, group packagebundler test
    
    * fix: always include the Project.toml in the appbundle
    
    create_pkg_context generates the Project.toml, but is currently called
    after the files have been copied over to the temporary directory that
    gets tarred up. This patch moves the copying of the appbundle contents
    after the Project.toml has been created.
    
    * fix: support relative paths in appbundle paths
    
    * docs: clarify the appbundle() docstring
    
    * Add CHANGELOG, set version to 0.1.6

--- test/packagebundler.jl
+++ test/packagebundler.jl
@@ -14,40 +14,210 @@ if !isdir(joinpath(pkg1, ".git"))
     touch(joinpath(pkg1, ".git", "test"))
 end
 
-bundle_env = joinpath(pkg1, "bin")
+@testset "bundle.standard" begin
+    bundle = "bundle.standard"
+    bundle_env = joinpath(pkg1, bundle)
+    out = tempname()
+    JuliaHub._PackageBundler.bundle(
+        bundle_env;
+        output=out,
+        verbose=false,
+    )
+    dir = mktempdir()
+    Tar.extract(out, dir)
 
-out = tempname()
-JuliaHub._PackageBundler.bundle(
-    bundle_env;
-    output=out,
-    verbose=false,
-)
-dir = mktempdir()
-Tar.extract(out, dir)
+    @test isfile(joinpath(dir, bundle, "Manifest.toml"))
+    @test isfile(joinpath(dir, bundle, "Project.toml"))
+    @test isfile(joinpath(dir, bundle, "run.jl"))
+    @test isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg1", "src", "Pkg1.jl"))
+    @test isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg2", "src", "Pkg2.jl"))
+    @test isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "src", "Pkg3.jl"))
 
-@test isfile(joinpath(dir, "bin", "Manifest.toml"))
-@test isfile(joinpath(dir, "bin", "Project.toml"))
-@test isfile(joinpath(dir, "bin", "run.jl"))
-@test isfile(joinpath(dir, "bin", ".bundle", "dev", "Pkg1", "src", "Pkg1.jl"))
-@test isfile(joinpath(dir, "bin", ".bundle", "dev", "Pkg2", "src", "Pkg2.jl"))
-@test isfile(joinpath(dir, "bin", ".bundle", "dev", "Pkg3", "src", "Pkg3.jl"))
+    @test !isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg2", "test", "blah"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg2", "test", "blah", "test"))
+    @test !isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg2", "test", "blub"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg2", "test", "blub", "test"))
 
-@test !isdir(joinpath(dir, "bin", ".bundle", "dev", "Pkg2", "test", "blah"))
-@test !isfile(joinpath(dir, "bin", ".bundle", "dev", "Pkg2", "test", "blah", "test"))
-@test !isdir(joinpath(dir, "bin", ".bundle", "dev", "Pkg2", "test", "blub"))
-@test !isfile(joinpath(dir, "bin", ".bundle", "dev", "Pkg2", "test", "blub", "test"))
+    @test isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "src", "bar"))
+    @test !isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "bar"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "bar", "test"))
 
-@test isfile(joinpath(dir, "bin", ".bundle", "dev", "Pkg3", "src", "bar"))
-@test !isdir(joinpath(dir, "bin", ".bundle", "dev", "Pkg3", "test", "bar"))
-@test !isfile(joinpath(dir, "bin", ".bundle", "dev", "Pkg3", "test", "bar", "test"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "src", "foo"))
+    @test !isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "foo"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "foo", "test"))
+    @test isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "src", "fooo"))
+    @test isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "fooo"))
+    @test isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "fooo", "test"))
 
-@test !isfile(joinpath(dir, "bin", ".bundle", "dev", "Pkg3", "src", "foo"))
-@test !isdir(joinpath(dir, "bin", ".bundle", "dev", "Pkg3", "test", "foo"))
-@test !isfile(joinpath(dir, "bin", ".bundle", "dev", "Pkg3", "test", "foo", "test"))
-@test isfile(joinpath(dir, "bin", ".bundle", "dev", "Pkg3", "src", "fooo"))
-@test isdir(joinpath(dir, "bin", ".bundle", "dev", "Pkg3", "test", "fooo"))
-@test isfile(joinpath(dir, "bin", ".bundle", "dev", "Pkg3", "test", "fooo", "test"))
+    @test !isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg1", ".git")) # always ignored
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg1", ".git", "test")) # always ignored
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg1", "README.md")) # in .juliabundleignore
+end
 
-@test !isdir(joinpath(dir, "bin", ".bundle", "dev", "Pkg1", ".git")) # always ignored
-@test !isfile(joinpath(dir, "bin", ".bundle", "dev", "Pkg1", ".git", "test")) # always ignored
-@test !isfile(joinpath(dir, "bin", ".bundle", "dev", "Pkg1", "README.md")) # in .juliabundleignore
+@testset "bundle.noproject-throw" begin
+    bundle = "bundle.noproject-throw"
+    bundle_env = joinpath(pkg1, bundle)
+    out = tempname()
+    expected_error = (VERSION > v"1.8") ? "Could not find project at" : ErrorException
+    @test_throws expected_error JuliaHub._PackageBundler.bundle(
+        bundle_env;
+        output=out,
+        verbose=false,
+        # using default allownoenv=false
+    )
+end
+
+@testset "bundle.noproject" begin
+    bundle = "bundle.noproject"
+    bundle_env = joinpath(pkg1, bundle)
+    out = tempname()
+    JuliaHub._PackageBundler.bundle(
+        bundle_env;
+        output=out,
+        verbose=false,
+        allownoenv=true,
+    )
+    dir = mktempdir()
+    Tar.extract(out, dir)
+
+    @test isfile(joinpath(dir, bundle, "Manifest.toml"))
+    @test isfile(joinpath(dir, bundle, "Project.toml"))
+    @test isfile(joinpath(dir, bundle, "run.jl"))
+    @test isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg1", "src", "Pkg1.jl"))
+    @test isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg2", "src", "Pkg2.jl"))
+    @test isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "src", "Pkg3.jl"))
+
+    @test !isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg2", "test", "blah"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg2", "test", "blah", "test"))
+    @test !isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg2", "test", "blub"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg2", "test", "blub", "test"))
+
+    @test isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "src", "bar"))
+    @test !isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "bar"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "bar", "test"))
+
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "src", "foo"))
+    @test !isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "foo"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "foo", "test"))
+    @test isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "src", "fooo"))
+    @test isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "fooo"))
+    @test isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "fooo", "test"))
+
+    @test !isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg1", ".git")) # always ignored
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg1", ".git", "test")) # always ignored
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg1", "README.md")) # in .juliabundleignore
+end
+
+# This test does not include any of the `Pkg*` packages as dependencies (since we can't
+# resolve them based on the Project.toml alone).
+@testset "bundle.nomanifest" begin
+    bundle = "bundle.nomanifest"
+    bundle_env = joinpath(pkg1, bundle)
+    out = tempname()
+    @test_logs (:warn,) JuliaHub._PackageBundler.bundle(
+        bundle_env;
+        output=out,
+        verbose=false,
+    )
+    dir = mktempdir()
+    Tar.extract(out, dir)
+
+    @test isfile(joinpath(dir, bundle, "Manifest.toml"))
+    @test isfile(joinpath(dir, bundle, "Project.toml"))
+    @test isfile(joinpath(dir, bundle, "run.jl"))
+
+    # None of the dependencies will be copied over in this test.
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg1", "src", "Pkg1.jl"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg2", "src", "Pkg2.jl"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "src", "Pkg3.jl"))
+
+    @test !isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg2", "test", "blah"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg2", "test", "blah", "test"))
+    @test !isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg2", "test", "blub"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg2", "test", "blub", "test"))
+
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "src", "bar"))
+    @test !isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "bar"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "bar", "test"))
+
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "src", "foo"))
+    @test !isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "foo"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "foo", "test"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "src", "fooo"))
+    @test !isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "fooo"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "fooo", "test"))
+
+    @test !isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg1", ".git")) # always ignored
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg1", ".git", "test")) # always ignored
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg1", "README.md")) # in .juliabundleignore
+end
+
+@testset "bundle.noenv-throw" begin
+    bundle = "bundle.noenv-throw"
+    bundle_env = joinpath(pkg1, bundle)
+    out = tempname()
+    expected_error = (VERSION > v"1.8") ? "Could not find project at" : ErrorException
+    @test_throws expected_error JuliaHub._PackageBundler.bundle(
+        bundle_env;
+        output=out,
+        verbose=false,
+        # using default allownoenv=false
+    )
+end
+
+@testset "bundle.noenv" begin
+    bundle = "bundle.noenv"
+    bundle_env = joinpath(pkg1, bundle)
+    out = tempname()
+    @test_logs (:warn,) JuliaHub._PackageBundler.bundle(
+        bundle_env;
+        output=out,
+        verbose=false,
+        allownoenv=true
+    )
+    dir = mktempdir()
+    Tar.extract(out, dir)
+
+    @test isfile(joinpath(dir, bundle, "Manifest.toml"))
+    @test isfile(joinpath(dir, bundle, "Project.toml"))
+    @test isfile(joinpath(dir, bundle, "run.jl"))
+
+    # None of the dependencies will be copied over in this test.
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg1", "src", "Pkg1.jl"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg2", "src", "Pkg2.jl"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "src", "Pkg3.jl"))
+
+    @test !isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg2", "test", "blah"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg2", "test", "blah", "test"))
+    @test !isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg2", "test", "blub"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg2", "test", "blub", "test"))
+
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "src", "bar"))
+    @test !isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "bar"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "bar", "test"))
+
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "src", "foo"))
+    @test !isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "foo"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "foo", "test"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "src", "fooo"))
+    @test !isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "fooo"))
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg3", "test", "fooo", "test"))
+
+    @test !isdir(joinpath(dir, bundle, ".bundle", "dev", "Pkg1", ".git")) # always ignored
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg1", ".git", "test")) # always ignored
+    @test !isfile(joinpath(dir, bundle, ".bundle", "dev", "Pkg1", "README.md")) # in .juliabundleignore
+end
+
+@testset "bundle.standard (relative path)" begin
+    bundle = "bundle.standard"
+    bundle_env = joinpath(pkg1, bundle)
+    out = tempname()
+    cd(bundle_env) do
+        JuliaHub._PackageBundler.bundle(
+            ".";
+            output=out,
+            verbose=false,
+        )
+    end
+    @test isfile(out)
+end
