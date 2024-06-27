c01bf49b03f0e627d8ebc8e74b2ff49f623ddae5
Morten Piibeleht <morten.piibeleht@juliahub.com>
2024-06-27 15:03:06 +1200

    fix tests

--- test/jobs.jl
+++ test/jobs.jl
@@ -925,7 +925,7 @@ JuliaHub._OPTION_LoggingMode[] = JuliaHub._LoggingMode.NOKAFKA
         let jc = JuliaHub.submit_job(JuliaHub.script""; dryrun=true)
             @test jc.exposed_port === nothing
         end
-        @testset "port $port" for port in (1025, 8080, 9008, 9010, 18000, 24_399, 23_500, 31_767)
+        @testset "port $port" for port in (1025, 8080, 9008, 9010, 18000, 23_399, 23_500, 31_767)
             jc = JuliaHub.submit_job(JuliaHub.script""; expose=port, dryrun=true)
             @test jc.exposed_port == port
         end
@@ -934,8 +934,8 @@ JuliaHub._OPTION_LoggingMode[] = JuliaHub._LoggingMode.NOKAFKA
         @test_throws ArgumentError JuliaHub.submit_job(JuliaHub.script"", expose=443)
         @test_throws ArgumentError JuliaHub.submit_job(JuliaHub.script"", expose=1024)
         @test_throws ArgumentError JuliaHub.submit_job(JuliaHub.script"", expose=9009)
-        @test_throws ArgumentError JuliaHub.submit_job(JuliaHub.script"", expose=24_400)
-        @test_throws ArgumentError JuliaHub.submit_job(JuliaHub.script"", expose=24_499)
+        @test_throws ArgumentError JuliaHub.submit_job(JuliaHub.script"", expose=23_400)
+        @test_throws ArgumentError JuliaHub.submit_job(JuliaHub.script"", expose=23_499)
         @test_throws ArgumentError JuliaHub.submit_job(JuliaHub.script"", expose=31_768)
         @test_throws ArgumentError JuliaHub.submit_job(JuliaHub.script"", expose=40_000)
         @test_throws ArgumentError JuliaHub.submit_job(JuliaHub.script"", expose=-200)
