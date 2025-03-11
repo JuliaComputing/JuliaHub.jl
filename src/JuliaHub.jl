af8d3957fae709951ca01db4f1850a457ef5c7a2
Morten Piibeleht <morten.piibeleht@juliahub.com>
2025-03-11 18:36:35 +1300

    refactor: mark public names as public

--- src/JuliaHub.jl
+++ src/JuliaHub.jl
@@ -38,4 +38,28 @@ function __init__()
     _LOCAL_TZ[] = _localtz()
 end
 
+# JuliaHub.jl follows the convention that all private names are
+# prefixed with an underscore.
+function _find_public_names()
+    return filter(names(@__MODULE__; all=true)) do s
+        # Internal functions and types, prefixed by _
+        startswith(string(s), "_") && return false
+        # Internal macros, prefixed by _
+        startswith(string(s), "@_") && return false
+        # Strange generated functions
+        startswith(string(s), "#") && return false
+        # Some core functions that are not relevant for the package
+        s in [:eval, :include] && return false
+        return true
+    end
+end
+macro _mark_names_public()
+    if !Base.isdefined(Base, :ispublic)
+        return nothing
+    end
+    public_names = _find_public_names()
+    return esc(Expr(:public, public_names...))
+end
+@_mark_names_public
+
 end
